71f382c683fbe65d1256724b82f635363420be8d
= <=>
2022-04-18 09:00:46 -0500

    build

---
+++ PostgresTesting/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace UnitTestProject;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
